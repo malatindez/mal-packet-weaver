d7a601f04ead6ce559e8be6d263ba233db530b44
malatindez <axenance@gmail.com>
2023-08-14 15:14:43 +0300

    add await_future functions

--- mal-packet-weaver/mal-packet-weaver/common.hpp
+++ mal-packet-weaver/mal-packet-weaver/common.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #if defined(_WIN32)
 #include <SDKDDKVer.h>
 #endif
@@ -46,4 +47,71 @@ namespace mal_packet_weaver
      * them with . This can help improve code readability and simplify usage.
      */
     using namespace mal_toolkit;
+
+    /**
+     * @brief Awaiting an std::future with Boost.Asio integration.
+     * 
+     * This function allows you to co_await an std::future inside a Boost.Asio coroutine, 
+     * waiting for the future's result and providing non-blocking behavior.
+     * 
+     * @tparam T The type of the awaited value.
+     * @tparam Executor The executor type that will be used for the coroutine.
+     * 
+     * @param fut The std::future object to be awaited.
+     * @param ex The executor for the coroutine.
+     * 
+     * @return A Boost.Asio awaitable representing the result of the future.
+     */
+    template <typename T, typename Executor = boost::asio::any_io_executor>
+    boost::asio::awaitable<T, Executor> await_future(std::future<T>& fut, Executor ex) {
+        while (fut.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+        {
+            boost::asio::steady_timer timer(ex, std::chrono::microseconds(10));
+            co_await timer.async_wait(boost::asio::use_awaitable);
+        }
+
+        try {
+            co_return fut.get();
+        } catch (const std::exception& e) {
+            std::rethrow_exception(std::current_exception());
+        }
+    }
+
+    /**
+     * @brief Awaiting an std::future with timeout and Boost.Asio integration.
+     * 
+     * This function allows you to co_await an std::future inside a Boost.Asio coroutine,
+     * waiting for the future's result with a specified timeout, and providing non-blocking behavior.
+     * 
+     * @tparam T The type of the awaited value.
+     * @tparam ChronoType The type of the timeout duration (e.g., std::chrono::milliseconds).
+     * @tparam Executor The executor type that will be used for the coroutine.
+     * 
+     * @param fut The std::future object to be awaited.
+     * @param ex The executor for the coroutine.
+     * @param timeout The maximum duration to wait for the future's result.
+     * 
+     * @return A Boost.Asio awaitable representing the result of the future.
+     * @throws std::runtime_error if the future is not ready within the specified timeout.
+     */
+    template <typename T, typename ChronoType, typename Executor = boost::asio::any_io_executor>
+    boost::asio::awaitable<T, Executor> await_future(std::future<T>& fut, Executor ex, ChronoType timeout)
+    {
+        auto start = std::chrono::steady_clock::now();
+
+        while (fut.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+            if (std::chrono::steady_clock::now() - start > timeout) {
+                throw std::runtime_error("Future not ready within timeout");
+            }
+            boost::asio::steady_timer timer(ex, std::chrono::microseconds(10));
+            co_await timer.async_wait(boost::asio::use_awaitable);
+        }
+
+        try {
+            co_return fut.get();
+        } catch (const std::exception& e) {
+            std::rethrow_exception(std::current_exception());
+        }
+    }
+
 }  // namespace mal_packet_weaver
